c9b72b0321a81bc008d5801c91d7d8c99132f155
TheSuryah <sattisuryasaiprakashreddy@gmail.com>
2021-09-11 17:09:24 +0530

    Menu direction change

--- js.js
+++ js.js
@@ -19,9 +19,9 @@ function scrollFunction() {
 }
 
 function openMenu() {
-  document.getElementById("menu").style.height = "100%";
+  document.getElementById("menu").style.width = "100%";
 }
 
 function closeMenu() {
-  document.getElementById("menu").style.height = "0%";
+  document.getElementById("menu").style.width = "0%";
 }
