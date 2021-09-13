689e478879f1192c4ddaac1a8d45e5db2d146426
TheSuryah <sattisuryasaiprakashreddy@gmail.com>
2021-09-13 09:50:53 +0530

    Cards

--- js.js
+++ js.js
@@ -3,15 +3,12 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("navbar").style.backgroundColor = "#111111";
-  } else {
-    document.getElementById("navbar").style.backgroundColor = "transparent";
-  }
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("mobile-navbar").style.backgroundColor = "#111111";
   } else {
+    document.getElementById("navbar").style.backgroundColor = "transparent";
     document.getElementById("mobile-navbar").style.backgroundColor = "transparent";
   }
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
     document.getElementById("glimpse").style.opacity = "1";
   } else {
     document.getElementById("glimpse").style.opacity = "0";
